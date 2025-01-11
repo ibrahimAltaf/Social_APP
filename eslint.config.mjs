974e1ffbd727eb423edd861ddcb688c49546fc64
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-01-10 16:46:26 -0800

    """""

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,14 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
-export default [
-    ...compat.config({
-        extends: ["next/core-web-vitals"],
-        parserOptions: {
-            ecmaVersion: 2021,
-        },
-    }),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
