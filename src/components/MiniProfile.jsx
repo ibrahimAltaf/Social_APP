64c36c7e4280105d11873d1ca5e307324e8ceb6f
ibrahimAltaf <muhammadibrahimaltaf312@gmail.com>
2025-01-13 08:00:05 -0800

    "dev"

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -4,9 +4,9 @@ import { useUser ,UserButton } from '@clerk/nextjs'
 export default function MiniProfile() {
   const {user} = useUser()
 
-  console.log(`user=======>>> ${JSON.stringify(user && user.firstName)}`)
+  console.log(`user=======>>> ${JSON.stringify(user )}`)
     return (
-    <div className='flex'>
+    <div className=''>
 
         <UserButton />
         <h3>{user && user.firstName}</h3>
