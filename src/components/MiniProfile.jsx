20770948cba7acdf5b62a044f1dd1e8ce1c53b1f
ibrahimAltaf <muhammadibrahimaltaf312@gmail.com>
2025-01-13 07:41:21 -0800

    """""

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,13 +1,15 @@
 "use client"
 import React from 'react'
-import { useUser } from '@clerk/nextjs'
+import { useUser ,UserButton } from '@clerk/nextjs'
 export default function MiniProfile() {
   const user = useUser()
 
   console.log(`user=======>>> ${JSON.stringify(user)}`)
     return (
-    <div>
-      Profile
+    <div className='flex'>
+
+        <UserButton />
+        <h3>{user.username}</h3>
     </div>
   )
 }
