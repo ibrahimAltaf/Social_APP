927a7c5aba3fe4d10c1efd0104347cef0e5feb60
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-01-15 15:41:37 -0800

    sa

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -12,11 +12,7 @@ export default function MiniProfile() {
   return (
     <div className="flex items-center p-4 bg-white shadow-md rounded-lg w-72">
         <UserButton />
-      <img 
-        src={user.imageUrl} 
-        alt="Profile Picture" 
-        className="w-16 h-16 rounded-full border-2 border-blue-500"
-      />
+    
       <div className="ml-4">
         <h2 className="text-lg font-semibold">{user.firstName} {user.lastName}</h2>
         <p className="text-sm text-gray-600">{user.emailAddresses[0].emailAddress}</p>
