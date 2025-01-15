ad3873453fad433f5b903dc0b9df413f89666c60
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-01-15 15:23:42 -0800

    ssdsd

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,8 +12,7 @@ export const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: 'SWIFT_SOCIAL',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  
     });
     console.log('Connected to MongoDB');
     initialized = true;
