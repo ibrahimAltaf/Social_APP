8246c7d42bb4312a79300e7a14435d12ab80647d
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-01-27 20:27:25 -0300

    ""useRef""

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,20 +2,23 @@
 import React from 'react'
 import { FaImage } from "react-icons/fa";
 import { useUser } from '@clerk/nextjs';
+import { useRef } from 'react';
 export default function Input() {
  const { user ,isSignedIn,isLoaded} = useUser();
+ const IMAGEFILEREF = useRef(null)
 if(!user || !isSignedIn || !isLoaded){
     return null
 }
 
   return (
     <div className='flex  '> 
+        <img src={user.imageUrl} alt='user_img' />
       <textarea noOfLines={2}/>
       
       <div className="">
-      <FaImage />
-      <input type='file' accept='images/*' />
-      
+      <FaImage /> 
+      <input hidden  type='file' accept='images/*' ref={IMAGEFILEREF} />
+
       <button>Post</button>
       </div>
     </div>
