ee8c1000b8359afb7137cb7bf17a3cfd705d4b97
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-01-27 17:03:39 -0300

    "New DEv"

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,16 +1,23 @@
 "use client"
 import { useUser } from '@clerk/nextjs'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { HiOutlinePhoto } from "react-icons/hi2";
 
 export default function Input() {
     const { user ,isSignedIn,isLoaded} = useUser()
     const ImagePickRef =useRef(null)
+    const [imageFileUrl,SetImageFileURl]=useState(null)
+    const [SelectedFile,SetSelectedFile]=useState(null)
+    const [imageFileUploading,setImageFileUploading]=useState(false)
     if(!isLoaded || !isSignedIn) {
         return null
     }
-const addImageToPost =()=>{
-
+const addImageToPost =(e)=>{
+const file = e.target.files[0]
+if(file){
+    SetSelectedFile(file)
+    setImageFileUploading(URL.createObjectURL(file))
+}
 }
   return (
     <div className='flex border-b border-gray-200 p-3 space-x3 w-full'>
@@ -18,6 +25,15 @@ const addImageToPost =()=>{
         <img src={user.imageUrl} alt="" className='w-12 h-12 rounded-full cursor-pointer hover:brightness-95 object-cover'/>
         <div className="w-full divide-y divide-gray-200">
         <textarea rows={"2"} placeholder='What is happening?' className='w-full outline-none tracking-wide min-h-[50px] text-gray-700' />
+        {SelectedFile && (
+            <img 
+  src={imageFileUrl} 
+  alt="selected-img" 
+  className={`w-full max-h-[250px] object-cover cursor-pointer ${imageFileUploading ? "animate-pulse" : ""}`} 
+/>
+        )
+
+        }
         <div className="flex items-center justify-between pt-2.5  ">
             <HiOutlinePhoto className='w-10 h-10 text-blue-500 cursor-pointer hover:bg-sky-100 rounded-full cursor-pointer' onClick={()=>{
                 ImagePickRef.current.click()
