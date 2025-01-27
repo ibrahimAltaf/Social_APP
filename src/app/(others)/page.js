6f6dbd0afcfc2d2b3ccea95991b5a2e0c4bfdbf3
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-01-27 20:18:55 -0300

    ""ui""

--- src/app/(others)/page.js
+++ src/app/(others)/page.js
@@ -8,7 +8,7 @@ export default function page() {
     <div className='min-h-screen max-w-xl mx-auto border-r border-l '>
       <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200 '>
         <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
-    <Input/>
+<Input/>
       </div>
     </div>
   )
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,23 @@
+"use client"
 import React from 'react'
-
+import { FaImage } from "react-icons/fa";
+import { useUser } from '@clerk/nextjs';
 export default function Input() {
+ const { user ,isSignedIn,isLoaded} = useUser();
+if(!user || !isSignedIn || !isLoaded){
+    return null
+}
+
   return (
-    <div>
+    <div className='flex  '> 
+      <textarea noOfLines={2}/>
       
+      <div className="">
+      <FaImage />
+      <input type='file' accept='images/*' />
+      
+      <button>Post</button>
+      </div>
     </div>
   )
 }
