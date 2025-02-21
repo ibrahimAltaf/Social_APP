e1825085d7428d0f29ad122f0496db541d0bc126
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-02-21 19:42:21 -0300

    ""dat"

--- src/lib/models/post.model.js
+++ src/lib/models/post.model.js
@@ -11,7 +11,6 @@ const POSTSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
     },
     name: {
         type: String,
