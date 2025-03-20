9585ff3d718ae72b463e89008304bede90f295e7
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-03-20 06:40:01 -0300

    "all set"

--- src/app/(others)/page.js
+++ src/app/(others)/page.js
@@ -1,28 +1,32 @@
-"use client"
-import Feed from '../../components/Feed'
-import Input from '../../components/Input'
-import React from 'react'
+"use client";
+import Input from "../../components/Input";
+import React, { useEffect, useState } from "react";
 
+export default async function Home() {
+let data = null
+try {
+  const result = await fetch("https://social-app-two-gold.vercel.app/api/post/all", {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}) 
+  });
+  data = await result.json()
+} catch (error) {
+  console.log(error + "error in get post api")
+}
 
-export default async function page() {
-  let data = null 
-  try {
-    const result = await fetch('https://social-app-two-gold.vercel.app/api/post/all' ,{
-      method:"POST",
-      cache: "no-store",
-    })
-    data = await result.json()
-  } catch (error) {
-    console.log(`Error in fecthing post`,error)
-    
-  }
+ console.log("api post data " + data)
+
   return (
-    <div className='min-h-screen max-w-xl mx-auto border-r border-l '>
-      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200 '>
-        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
-<Input/>
-
+    <div className="min-h-screen max-w-xl mx-auto border-r border-l">
+      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold">Home</h2>
+        <Input />
       </div>
+      
     </div>
-  )
+  );
 }
