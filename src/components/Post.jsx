6f2f214ff3c334dc7e25497df1de9cf6908235d9
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-03-20 06:06:31 -0300

    "all set""

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,7 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
-import moment from "moment"
+// import moment from "moment"
 const Post = ({post}) => {
   return (
     <div className='flex p-3 border-b border-gray-200 w-full hover:bg-gray-50'>
@@ -17,7 +18,7 @@ const Post = ({post}) => {
                     <h4 className='font-bold textxs truncate max-w-32'>{post?.name}</h4>
                     <span>@{post?.username}</span>
                     <span className='text-xl text-gray-500'>.</span>
-                    <span className='text-xs text-gray-500 flex-1 truncate '>{moment(post?.createdAt).fromNov()}</span>
+                    {/* <span className='text-xs text-gray-500 flex-1 truncate '>{moment(post?.createdAt).fromNov()}</span> */}
                 </div>
             </div>
         </div>
