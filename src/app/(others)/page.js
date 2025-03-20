b227ea712976feead089e13004fc9a3e6bf25740
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-03-20 06:40:54 -0300

    "all set"

--- src/app/(others)/page.js
+++ src/app/(others)/page.js
@@ -3,22 +3,7 @@ import Input from "../../components/Input";
 import React, { useEffect, useState } from "react";
 
 export default async function Home() {
-let data = null
-try {
-  const result = await fetch("https://social-app-two-gold.vercel.app/api/post/all", {
-    method: "POST",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}) 
-  });
-  data = await result.json()
-} catch (error) {
-  console.log(error + "error in get post api")
-}
 
- console.log("api post data " + data)
 
   return (
     <div className="min-h-screen max-w-xl mx-auto border-r border-l">
