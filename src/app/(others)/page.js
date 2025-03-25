eaaa5584caafb51a6943e74289216d1381fd205d
IbrahimAltaf <117311069+ibrahimAltaf@users.noreply.github.com>
2025-03-25 22:06:11 +0500

    "all new set"

--- src/app/(others)/page.js
+++ src/app/(others)/page.js
@@ -3,37 +3,24 @@ import Feed from "../../components/Feed";
 import Input from "../../components/Input";
 import React, { useEffect, useState } from "react";
 
-export default function Home() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const getAllPost = async () => {
-      try {
-        const result = await fetch("https://social-app-two-gold.vercel.app/api/post/all", {
-          method: "POST",
-          cache: "no-cache",
-        });
-  
-       
-        const jsonData = await result.json();
-        console.log("Fetched Data:", jsonData);
-        setData(jsonData); 
-      } catch (error) {
-        console.log("Error fetching posts:", error);
-      }
-    };
-  
-    getAllPost();
-  }, []);
-  console.log("page data set ===>" + data)
-
+export default async function Home() {
+  let data = null;
+  try {
+    const result = await fetch(process.env.URL + '/api/post/all', {
+      method: 'POST',
+      cache: 'no-store',
+    });
+    data = await result.json();
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
   return (
-    <div className="min-h-screen max-w-xl mx-auto border-r border-l">
-      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
-        <h2 className="text-lg sm:text-xl font-bold">Home</h2>
-        <Input />
+    <div className='min-h-screen max-w-xl mx-auto border-r border-l'>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
       </div>
-<Feed data={data}/>
-      
+      <Input />
+      <Feed data={data} />
     </div>
   );
 }
